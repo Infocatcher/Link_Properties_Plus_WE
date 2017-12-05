73b145fc5d15d79dfbb50ef4738e3126ad50b58e
Infocatcher <infocatcher.dev.null@gmail.com>
2017-12-05 14:46:44 +0300

    Shortify code in properties window

--- properties.js
+++ properties.js
@@ -1,23 +1,18 @@
-var lppOptions = {
-	init: function() {
-		var params = new URL(location).searchParams;
-		$("url").value = params.get("url");
-		$("referer").value = params.get("referer");
-		addEventListener("beforeunload", function() { // Note: can't save on unload
-			browser.storage.local.set({
-				windowPosition: {
-					x: screenX,
-					y: screenY,
-					w: outerWidth,
-					h: outerHeight
-				}
-			});
-		}, { once: true });
-	}
-};
+var params = new URL(location).searchParams;
+$("url").value = params.get("url");
+$("referer").value = params.get("referer");
+
+addEventListener("beforeunload", function() { // Note: can't save on unload
+	browser.storage.local.set({
+		windowPosition: {
+			x: screenX,
+			y: screenY,
+			w: outerWidth,
+			h: outerHeight
+		}
+	});
+}, { once: true });
 
 function $(id) {
 	return document.getElementById(id);
 }
-
-lppOptions.init();
