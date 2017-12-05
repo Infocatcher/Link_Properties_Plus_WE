36117ccefea3de1e40dc3060deb8e906a22197f3
Infocatcher <infocatcher.dev.null@gmail.com>
2017-12-05 17:05:22 +0300

    Draft for buttons in properties window

--- properties.js
+++ properties.js
@@ -2,6 +2,13 @@ var params = new URL(location).searchParams;
 $("url").value = params.get("url");
 $("referer").value = params.get("referer");
 
+var handlers = {
+	get: getProperties,
+	options: openOptions
+};
+for(var id in handlers)
+	$(id).addEventListener("click", handlers[id]);
+
 addEventListener("beforeunload", function() { // Note: can't save on unload
 	browser.storage.local.set({
 		windowPosition: {
@@ -12,6 +19,16 @@ addEventListener("beforeunload", function() { // Note: can't save on unload
 		}
 	});
 }, { once: true });
+addEventListener("unload", function() {
+	for(var id in handlers)
+		$(id).removeEventListener("click", handlers[id]);
+}, { once: true });
+
+function getProperties() {
+}
+function openOptions() {
+	browser.runtime.openOptionsPage();
+}
 
 function $(id) {
 	return document.getElementById(id);
