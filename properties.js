0c2710e64211ed6d8aced6c6d8d616402e345d1e
Infocatcher <infocatcher.dev.null@gmail.com>
2017-12-06 17:33:16 +0300

    Basic implementation to send HEAD request

--- properties.js
+++ properties.js
@@ -25,7 +25,40 @@ addEventListener("unload", function() {
 }, { once: true });
 
 function getProperties() {
+	var url = $("url").value;
+	var referer = $("referer").value;
+	sendRequest(url, referer);
 }
+function sendRequest(url, referer) {
+	var request = new XMLHttpRequest();
+	request.open("HEAD", url, true);
+	// Doesn't work: Attempt to set a forbidden header was denied: Referer
+	referer && request.setRequestHeader("Referer", referer);
+	request.send();
+	request.onreadystatechange = function() {
+		if(this.readyState == this.HEADERS_RECEIVED) {
+			showProperties(request);
+			request.abort();
+		}
+	};
+}
+function showProperties(request) {
+	var size = request.getResponseHeader("Content-Length");
+	$("size").textContent = size;
+
+	var date = request.getResponseHeader("Last-Modified");
+	$("date").textContent = date;
+
+	var type = request.getResponseHeader("Content-Type");
+	$("type").textContent = type;
+
+	var status = request.status;
+	$("status").textContent = status;
+
+	var direct = request.responseURL;
+	$("direct").textContent = direct;
+}
+
 function openOptions() {
 	browser.runtime.openOptionsPage();
 }
