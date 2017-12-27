30fc91a7b9bf5a3b6de8baff223c070fd80ea525
Infocatcher <infocatcher.dev.null@gmail.com>
2017-12-27 22:09:39 +0300

    Draft for options page

---
+++ options.js
@@ -0,0 +1,40 @@
+function init() {
+	readPrefs(loadOptions);
+	addEventListener("input", saveOption);
+	addEventListener("unload", destroy, { once: true });
+}
+function destroy() {
+	removeEventListener("input", saveOption);
+}
+
+function loadOptions() {
+	for(var id in prefs)
+		loadOption(id, prefs[id]);
+}
+function loadOption(id, val) {
+	var node = document.getElementById(id);
+	node && setValue(node, val);
+}
+function saveOption(e) {
+	var node = e.target;
+	if(!(node.id in prefs))
+		return;
+	browser.storage.local.set({
+		[node.id]: getValue(node)
+	});
+}
+function getValue(node) {
+	return node.localName == "select" || node.type == "number"
+		? +node.value
+		: node.type == "checkbox"
+			? node.checked
+			: node.value;
+}
+function setValue(node, val) {
+	if(node.type == "checkbox")
+		node.checked = val;
+	else
+		node.value = val;
+}
+
+init();
